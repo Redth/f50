410864289723ffec2019324c955a146764b6959b
Jonathan Dick <jondick@gmail.com>
2018-01-29 11:00:22 -0500

    Comments to explain some additions

--- Sample/SampleServices.Android/MainActivity.cs
+++ Sample/SampleServices.Android/MainActivity.cs
@@ -20,6 +20,7 @@ namespace SampleServices.Droid
 
             base.OnCreate (bundle);
 
+            // One single simple init call which allows the android app to keep track of current activity/context
             global::Xamarin.Services.Platform.Init (this, bundle);
 
             global::Xamarin.Forms.Forms.Init (this, bundle);
@@ -27,11 +28,16 @@ namespace SampleServices.Droid
             LoadApplication (new App ());
         }
 
+        // This is required to handle permissions automatically for the user
         public override void OnRequestPermissionsResult (int requestCode, string[] permissions, Permission[] grantResults)
         {
             global::Xamarin.Services.Permissions.PermissionsService.Current.OnRequestPermissionsResult (requestCode, permissions, grantResults);
         }
 
+        // I don't think anyone ever really does this, and I'm not sure it's actually necessary, but I've added it
+        // for good measure.  Realistically if the app is torn down, I don't think we need to worry about this
+        // There's really no cleanup or saving of any sort of state that isn't reinstantiated when the app is created
+        // again anyway.
         protected override void OnDestroy ()
         {
             global::Xamarin.Services.Platform.Uninit ();
